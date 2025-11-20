6a67b06af254453aafa7356e8a4311e39133b449
laiyi-ohlsen <laiyi.ohlsen@gmail.com>
2025-11-20 17:38:29 -0500

    updated to wss

--- server/server.js
+++ server/server.js
@@ -34,9 +34,3 @@ const { WebSocketServer } = require('ws');
 
 const PORT = 3001; 
 server.listen(PORT, () => { console.log(`Server listening on http://localhost:${PORT}`)});
-
-// function writeSomething(somethingA,function somethingElse(somethingC) {
-
-// }){
-
-// }
